c14682fa1be42a89a62f58d36a49ad5298c28b12
xiften <6wn5seb9@anonaddy.me>
2023-06-09 06:36:42 -0600

    stopped linting for now

--- next.config.mjs
+++ next.config.mjs
@@ -6,29 +6,32 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-    experimental: {
-        esmExternals: false,
-    },
-    reactStrictMode: true,
+  experimental: {
+    esmExternals: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
 
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "**",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 
-    /**
-     * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-     * out.
-     *
-     * @see https://github.com/vercel/next.js/issues/41980
-     */
-    i18n: {
-        locales: ["en"],
-        defaultLocale: "en",
-    },
+  /**
+   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
+   * out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 };
 export default config;
